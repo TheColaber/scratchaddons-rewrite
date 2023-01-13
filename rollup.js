42a72d6855979e7db6fca63f6e2f510f232fce65
TheColaber <thecolaber@gmail.com>
2023-01-12 21:09:22 -0500

    Add console.logs for watcher

--- rollup.js
+++ rollup.js
@@ -5,13 +5,17 @@ const [, , arg] = process.argv;
 if (arg === "-w") {
   const watcher = watch(config);
   watcher.on("event", (event) => {
-    console.log(event.code);
+    if (event.code === "BUNDLE_START") {
+      console.log("Bundling...")
+    }
 
     if (event.code === "ERROR") {
       console.log(event.error);
     }
 
     if (event.code === "BUNDLE_END") {
+      console.log("Done in", event.duration, "ms", "- Watching for changes...");
+      console.log();
       event.result.close();
     }
   });
