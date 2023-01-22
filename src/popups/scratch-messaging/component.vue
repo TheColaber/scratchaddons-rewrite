4df90915914f541259c311d84238de5be2a10804
TheColaber <thecolaber@gmail.com>
2023-01-22 15:30:35 -0500

    make better css vars

--- src/popups/scratch-messaging/component.vue
+++ src/popups/scratch-messaging/component.vue
@@ -1,25 +1,17 @@
 <template>
   <div :class="$style.container">
     test
-
-    {{ storage }}
   </div>
 </template>
 
 <script lang="ts">
-const storagePromise = chrome.storage.sync.get("addonSettings");
 
 export default {
   props: ["manifest"],
   data() {
     return {
-      storage: {},
     };
-  },
-  async created() {
-    const { addonSettings = {} } = await storagePromise;
-    this.storage = addonSettings;
-  },
+  }
 };
 </script>
 
